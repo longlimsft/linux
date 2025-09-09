805a1acacc08d57d55da02f83b08cfec2d25170f
Long Li <longli@microsoft.com>
2025-09-08 18:52:14 -0700

    net:mana: Move NAPI from CQ to EQ
    With dedicated EQ created on each vPort and no longer shared between
    different net devices, move the NAPI code from CQ to EQ.
    
    This allows the driver to arm the EQ when NAPI is done, thus avoiding
    unncessary arming and results in less interrupts from the MANA device.

--- include/net/mana/mana.h
+++ include/net/mana/mana.h
@@ -113,8 +113,6 @@ struct mana_txq {
 
 	atomic_t pending_sends;
 
-	bool napi_initialized;
-
 	struct mana_stats_tx stats;
 };
 
@@ -265,8 +263,6 @@ struct mana_tx_comp_oob {
 
 struct mana_rxq;
 
-#define CQE_POLLING_BUFFER 512
-
 struct mana_cq {
 	struct gdma_queue *gdma_cq;
 
@@ -286,14 +282,10 @@ struct mana_cq {
 	 */
 	struct mana_txq *txq;
 
-	/* Buffer which the CQ handler can copy the CQE's into. */
-	struct gdma_comp gdma_comp_buf[CQE_POLLING_BUFFER];
+	/* Pointer to a buffer which the CQ handler can copy the CQE's into. */
+	struct gdma_comp *gdma_comp_buf;
 
-	/* NAPI data */
-	struct napi_struct napi;
-	int work_done;
-	int work_done_since_doorbell;
-	int budget;
+	int cqe_done_since_doorbell;
 };
 
 struct mana_recv_buf_oob {
